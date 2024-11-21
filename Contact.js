55e40cdafa8432394bcef42f3626609c6f78dce2
Leewen <leewen.steinmetz@epita.fr>
2024-11-21 12:42:08 +0100

    bfrJava

--- Contact.js
+++ Contact.js
@@ -4,3 +4,47 @@ hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
 })
+document.addEventListener('click', (event) => {
+    if (!offScreenMenu.contains(event.target) && !hamMenu.contains(event.target)) {
+        offScreenMenu.classList.remove('active');
+        hamMenu.classList.remove('active');
+    }
+});
+
+
+//const test = document.querySelector('.testJava');
+//test.addEventListener('click', () => {
+//    test.classList.toggle('active');
+//})
+
+const acceuil = document.querySelector('.acceuil');
+const apropos = document.querySelector('.apropos');
+const contactus = document.querySelector('.contactus');
+const inspire = document.querySelector('.inspire');
+acceuil.addEventListener('mouseenter', () => {
+    acceuil.classList.add('active');
+});
+acceuil.addEventListener('mouseleave', () => {
+    acceuil.classList.remove('active');
+});
+
+apropos.addEventListener('mouseenter', () => {
+    apropos.classList.add('active');
+});
+apropos.addEventListener('mouseleave', () => {
+    apropos.classList.remove('active');
+});
+
+contactus.addEventListener('mouseenter', () => {
+    contactus.classList.add('active');
+});
+contactus.addEventListener('mouseleave', () => {
+    contactus.classList.remove('active');
+});
+
+inspire.addEventListener('mouseenter', () => {
+    inspire.classList.add('active');
+});
+inspire.addEventListener('mouseleave', () => {
+    inspire.classList.remove('active');
+});
